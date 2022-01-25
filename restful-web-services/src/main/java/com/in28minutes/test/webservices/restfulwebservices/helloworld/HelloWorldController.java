8a140ce6126c8ae9c4ac153b7ee580acf5676824
BOONKUAE-MIS\johnk <john.kuae@gmail.com>
2022-01-25 21:27:04 +0700

    Internationalization for RESTful Services

--- restful-web-services/src/main/java/com/in28minutes/test/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/test/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.in28minutes.test.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -22,5 +32,12 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+//            @RequestHeader(name = "Accept-Language",required = false) Locale locale
+            ) {
+//        return messageSource.getMessage("hello.message",null,"Default Message",locale);
+        return messageSource.getMessage("hello.message",null,"Default Message", LocaleContextHolder.getLocale());
 
+    }
 }
