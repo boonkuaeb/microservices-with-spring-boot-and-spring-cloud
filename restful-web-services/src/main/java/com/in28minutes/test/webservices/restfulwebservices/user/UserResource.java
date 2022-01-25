98f477c0e547b7e1bc9ef1a642557d1fdd86aff0
BOONKUAE-MIS\johnk <john.kuae@gmail.com>
2022-01-25 20:56:09 +0700

    add hateos link

--- restful-web-services/src/main/java/com/in28minutes/test/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/test/webservices/restfulwebservices/user/UserResource.java
@@ -1,6 +1,10 @@
 package com.in28minutes.test.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -8,6 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
 
 @RestController
 public class UserResource {
@@ -21,12 +26,16 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
         if (user == null) {
             throw new UserNotFoundException("id-" + id);
         }
-        return user;
+        EntityModel<User> model = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkToUser = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkToUser.withRel("all-users"));
+        return model;
     }
 
     @PostMapping("/users")
