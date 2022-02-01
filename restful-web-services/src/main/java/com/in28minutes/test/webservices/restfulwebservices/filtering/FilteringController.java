260eef0c536744448e8f688c97ab77f3970d0a63
BOONKUAE-MIS\johnk <john.kuae@gmail.com>
2022-02-01 22:30:10 +0700

    Step 25 - Implementing Dynamic Filtering for RESTful Service

--- restful-web-services/src/main/java/com/in28minutes/test/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/in28minutes/test/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,26 +1,54 @@
 package com.in28minutes.test.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class FilteringController {
-
+    /**
+     * Filter only "field1","field2"
+     * @return MappingJacksonValue
+     */
     @GetMapping("/filtering")
-    public SomeBean retieveSomeBean()
+    public MappingJacksonValue retieveSomeBean()
     {
-        return new SomeBean("value1","value2","value3");
+        SomeBean someBean =  new SomeBean("value1","value2","value3");
+
+        MappingJacksonValue mapping = getMappingJacksonValue(someBean,"field1","field2");
+        return mapping;
     }
+
+    /**
+     * Filter only "field2","field3"
+     * @return MappingJacksonValue
+     */
     @GetMapping("/filtering-list")
-    public List<SomeBean> retieveListSomeBean()
+    public MappingJacksonValue retieveListSomeBean()
     {
-        return Arrays.asList(
+        List<SomeBean> list = Arrays.asList(
                 new SomeBean("value1","value2","value3"),
-                new SomeBean("value1","value2","value3")
-        );
+                new SomeBean("value1","value2","value3"));
+        MappingJacksonValue mapping = getMappingJacksonValue(list,"field2","field3");
+        return mapping;
     }
+
+    @NotNull
+    private MappingJacksonValue getMappingJacksonValue(Object list,String... propertyArray) {
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept(propertyArray);
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(list);
+        mapping.setFilters(filters);
+        return mapping;
+    }
+
+
 }
